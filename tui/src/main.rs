b42c49e2e66fe680aa974ef6edd3914ab02d132b
williameliasson-dev <williameliasson5@gmail.com>
2026-07-17 02:24:00 +0200

    stupid render shit need struct for views

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,20 +1,45 @@
 use ratatui::{DefaultTerminal, Frame};
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
-    ratatui::run(app)?;
-    Ok(())
+enum View {
+    Home,
+    Notes,
 }
 
-fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+struct App {
+    view: View,
+}
+
+impl App {
+    fn run_loop(&self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+        loop {
+            terminal.draw(|frame| self.render_view(frame, &self.view))?;
+            if crossterm::event::read()?.is_key_press() {
+                break Ok(());
+            }
         }
+    }
+
+    fn render_view(&self, frame: &mut Frame, view: &View) {
+        match view {
+            View::Home => frame.render_widget("home", frame.area()),
+            View::Notes => frame.render_widget("notes", frame.area()),
+        }
+    }
+
+    pub fn new() -> Self {
+        App { view: View::Home }
+    }
+
+    pub fn run(&self) -> Result<(), std::io::Error> {
+        ratatui::run(|terminal| self.run_loop(terminal))
     }
 }
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+fn main() -> color_eyre::Result<()> {
+    let app: App = App::new();
+
+    color_eyre::install()?;
+    app.run();
+
+    Ok(())
 }
