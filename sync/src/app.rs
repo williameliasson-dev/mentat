4fa88eaaa57e502d24292648df16d657c33cd174
williameliasson-dev <williameliasson5@gmail.com>
2026-08-30 21:22:14 +0200

    route

--- sync/src/app.rs
+++ sync/src/app.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
-use axum::Router;
+use axum::{Router, routing::post};
 
-use crate::Services;
+use crate::{Services, routes::account::create_account};
 
 pub fn app(services: Services) -> Router {
-    Router::new().with_state(Arc::new(services))
+    Router::new()
+        .with_state(Arc::new(services))
+        .route("account", post(create_account))
 }
--- sync/src/lib.rs
+++ sync/src/lib.rs
@@ -3,10 +3,11 @@ pub mod database;
 pub mod error;
 pub mod model;
 pub mod repository;
+pub mod routes;
 pub mod service;
 
 pub use database::Database;
-pub use model::Account;
 pub use error::{Result, SyncError};
+pub use model::Account;
 pub use repository::{AccountRepository, Repositories};
 pub use service::{AccountService, Services};
